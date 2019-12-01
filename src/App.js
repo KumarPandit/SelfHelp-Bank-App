193aa57389d136b60831c754a786f571e3886878
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2019-12-01 12:05:26 +1100

    Added bootstrap and imported styles. Closes #3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
-class App extends Component() {
+
+class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header"></header>
-      </div>
-    );
+    return <React.Fragment></React.Fragment>;
   }
 }
 
