ce23f3bcf9d9911735cbea26bbf04eb05a386fcc
Nabin Karki <naabin@outlook.com>
2019-12-01 22:41:44 +1100

    Added static navbar. Closes #7.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navigation from "./components/navbar/navigation";
 
 class App extends Component {
   render() {
     return <React.Fragment>
-      
+        <Navigation/>
     </React.Fragment>;
   }
 }
---
+++ src/components/navbar/navigation.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Toolbar, IconButton, Typography, Button, AppBar } from '@material-ui/core';
+
+
+
+const usetyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(5),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    navColor: {
+        position: 'relative',
+        backgroundColor: '#0b83e6'
+    }
+}));
+
+
+export default function Navigation() {
+    const classes = usetyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar className={classes.navColor}>
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"></IconButton>
+                    
+                    <Typography variant="h5" className={classes.title}>Dhukuti</Typography>
+                    <Button size="large" color="inherit">Feed</Button>
+                    <Button size="large" color="inherit">Signup</Button>
+                    <Button size="large" color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
