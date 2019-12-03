8ef689a9f7817f973c1d1fe96698e551f26fd1b3
Nabin Karki <naabin@outlook.com>
2019-12-04 00:55:45 +1100

    Fixed routing and tweaked the navigation bar. Closes #8.

--- src/components/navbar/navigation.js
+++ src/components/navbar/navigation.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./navigation.css";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, fade } from "@material-ui/core/styles";
 import {
   Toolbar,
-  IconButton,
   Typography,
-  TextField,
   Button,
-  AppBar
+  AppBar,
+  InputBase
 } from "@material-ui/core";
+
+import SearchIcon from '@material-ui/icons/Search';
 
 const usetyles = makeStyles(theme => ({
   root: {
@@ -23,7 +24,45 @@ const usetyles = makeStyles(theme => ({
   navColor: {
     position: "relative",
     backgroundColor: "#0b83e6"
-  }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    }
+  },
+
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
 }));
 
 export default function Navigation() {
@@ -31,23 +70,20 @@ export default function Navigation() {
 
   return (
     <div className={classes.root}>
-      <AppBar className={classes.navColor}>
+      <AppBar position="static" className={classes.navColor}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-
-          <Typography variant="h4" className={classes.title}>
-            Dhukuti
+          <Typography variant="h5" className={classes.title}>
+            DHUKUTI
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
           <div className="navbarlt">
-            <TextField id="standard-basic" label="Search" />
-            <Button size="large" color="inherit">
-              Search
-            </Button>
 
             <Button size="large" color="inherit">
               Feed
