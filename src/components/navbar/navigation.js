cb1b365702325c1cc7671c412f00f3fa04595326
Kumar Prasad Pandit <kumarpndt12@gmail.com>
2019-12-04 15:00:49 +1100

    created navBar and made it responsive closes #2

--- src/components/navbar/navigation.js
+++ src/components/navbar/navigation.js
@@ -1,102 +1,160 @@
 import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import MoreIcon from "@material-ui/icons/MoreVert";
 import "./navigation.css";
-import { makeStyles, fade } from "@material-ui/core/styles";
-import {
-  Toolbar,
-  Typography,
-  Button,
-  AppBar,
-  InputBase
-} from "@material-ui/core";
 
-import SearchIcon from '@material-ui/icons/Search';
-
-const usetyles = makeStyles(theme => ({
-  root: {
+const useStyles = makeStyles(theme => ({
+  grow: {
     flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(5)
+    marginRight: theme.spacing(1)
   },
   title: {
-    flexGrow: 1
-  },
-  navColor: {
-    position: "relative",
-    backgroundColor: "#0b83e6"
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    }
+    display: "block"
   },
 
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.35),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "55%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(10),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit"
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
-      },
-    },
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
   },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
 }));
 
 export default function Navigation() {
-  const classes = usetyles();
+  const classes = useStyles();
+
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem className={classes.menuButton}>
+        <Button color="inherit">FEED</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button color="inherit">ABOUT</Button>
+      </MenuItem>
+      <MenuItem>
+        <Button color="inherit">LOGIN</Button>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" className={classes.navColor}>
+    <div className={classes.grow}>
+      <AppBar position="static">
         <Toolbar>
-          <Typography variant="h5" className={classes.title}>
-            DHUKUTI
+          <Typography className={classes.title} variant="h6" noWrap>
+            DHUKUTI-APP
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
-            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              inputProps={{ 'aria-label': 'search' }}
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "search" }}
             />
           </div>
-          <div className="navbarlt">
-
-            <Button size="large" color="inherit">
-              Feed
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <Button size="small" color="inherit">
+              FEED
             </Button>
-            <Button size="large" color="inherit">
-              Signup
+            <Button size="small" color="inherit">
+              ABOUT
             </Button>
-            <Button size="large" color="inherit">
-              Login
+            <Button size="small" color="inherit">
+              LOGIN
+            </Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <Button
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
             </Button>
           </div>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
     </div>
   );
 }
